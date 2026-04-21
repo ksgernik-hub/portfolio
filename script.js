41476a15f501f8c5f363e7af7e382b291baa4d3b
ksgernik-hub <ks.gernik@gmail.com>
2026-04-21 17:02:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -36,3 +36,37 @@ projectLinks.forEach((link) => {
     }, 520);
   });
 });
+
+const otherProjectsPreview = document.querySelector('.other-projects__preview');
+const otherProjectsPreviewImage = otherProjectsPreview
+  ? otherProjectsPreview.querySelector('img')
+  : null;
+const otherProjectLinks = [...document.querySelectorAll('.other-project[data-preview]')];
+const desktopPreviewMedia = window.matchMedia('(hover: hover) and (pointer: fine) and (min-width: 981px)');
+
+if (otherProjectsPreview && otherProjectsPreviewImage && otherProjectLinks.length && desktopPreviewMedia.matches) {
+  const movePreview = (x, y) => {
+    const offsetX = 18;
+    const offsetY = 18;
+    otherProjectsPreview.style.transform = `translate3d(${x + offsetX}px, ${y + offsetY}px, 0)`;
+  };
+
+  otherProjectLinks.forEach((projectLink) => {
+    projectLink.addEventListener('mouseenter', () => {
+      const previewImage = projectLink.getAttribute('data-preview');
+      if (previewImage) {
+        otherProjectsPreviewImage.src = previewImage;
+      }
+      otherProjectsPreview.classList.add('is-visible');
+    });
+
+    projectLink.addEventListener('mousemove', (event) => {
+      movePreview(event.clientX, event.clientY);
+    });
+
+    projectLink.addEventListener('mouseleave', () => {
+      otherProjectsPreview.classList.remove('is-visible');
+      otherProjectsPreview.style.transform = 'translate3d(-9999px, -9999px, 0)';
+    });
+  });
+}
